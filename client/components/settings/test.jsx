3e24bfe1676ee573c640d720c8228e26d15d1047
Maddi Joyce <maddi@maddijoyce.com>
2016-06-07 15:12:14 +1000

    Test fail on travis.

--- client/components/settings/test.jsx
+++ client/components/settings/test.jsx
@@ -27,7 +27,7 @@ export default function (test) {
 
     mock.client.request.reset();
     element.find('button[value="Cancel"]').simulate('click');
-    assert.equal(mock.client.request.calledWith('setPage', 'home'), true,
+    assert.equal(mock.client.request.calledWith('setPage', 'home'), false,
       'Cancels to home page');
   });
 }
